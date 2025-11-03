1486bddadc96aa3c3f6784ea3c8623ab766c61f8
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-11-03 18:31:31 +0545

    working on hero section

--- src/components/Herosection/Herosection.tsx
+++ src/components/Herosection/Herosection.tsx
@@ -1,3 +1,6 @@
+'use client';
+import { motion } from 'framer-motion';
+
 const techWeUse = [
   {
     name: 'React',
@@ -41,22 +44,44 @@ const Herosection = () => {
         <h1 className="hero-title">
           Distinctive <br /> Web <span className="text-primary"> Design</span>
         </h1>
-        <div className="quick-tech-we-use">
+        <div className="quick-tech-we-use flex items-center gap-14">
           <span className=" block max-w-[300px] text-4xl leading-[1.3] text-body/80">
             Developing the Website for Tomorrow
           </span>
-          <ul className="tech-container">
+          <ul className="tech-container flex items-center">
             {techWeUse.map(tech => (
-              <li
+              <motion.li
                 key={tech.name}
-                className="inline-block">
-                <svg
-                  width={40}
-                  height={40}
-                  fill="currentColor">
+                className="tech-item relative w-[70px] h-[70px] flex items-center justify-center bg-primary rounded-full border-2 border-white -ml-2.5"
+                whileHover="hover"
+                initial="rest"
+                variants={{
+                  rest: {},
+                }}>
+                <motion.svg
+                  width={36}
+                  height={36}
+                  fill="currentColor"
+                  className="tech-icon"
+                  variants={{
+                    rest: { scale: 1 },
+                    hover: { scale: 1.1 },
+                  }}
+                  transition={{ duration: 0.3 }}>
                   <use xlinkHref={`/icons.svg#${tech.icon}`} />
-                </svg>
-              </li>
+                </motion.svg>
+
+                <motion.span
+                  className="tech-name absolute whitespace-nowrap px-2 py-1 rounded-md"
+                  variants={{
+                    rest: { opacity: 0, y: 10, pointerEvents: 'none' },
+                    hover: { opacity: 1, y: 0, pointerEvents: 'auto' },
+                  }}
+                  transition={{ duration: 0.3 }}
+                  style={{ top: '100%' }}>
+                  {tech.name}
+                </motion.span>
+              </motion.li>
             ))}
           </ul>
         </div>
