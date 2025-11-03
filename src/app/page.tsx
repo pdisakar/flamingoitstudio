aabc6f48ec204fb5c98d3a7dea1e621e73fa1db6
Sakar Pudasaini <pdi.sakar@gmail.com>
2025-11-03 12:40:27 +0545

    enviroment is done

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="container">
+      <div className="common-box">
+        <p className="text-primary font-primary">this is sakar</p>
+
+        <div className="third-font-text font-secondary font-extrabold text-6xl">
+          Flamingo IT Studio
+        </div>
+        <p>sakar pudasaini</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum animi impedit dolores perspiciatis aliquam alias aspernatur, debitis libero dignissimos. Id, dignissimos maxime exercitationem labore sequi aperiam cumque laborum inventore provident!</p>
+      </div>
+    </main>
+  );
+}
